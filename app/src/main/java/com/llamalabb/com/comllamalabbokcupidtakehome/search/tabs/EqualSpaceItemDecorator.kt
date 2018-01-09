47ce173e805f4b4e9526464b45c1dee8c7b80d78
Andy OBrien <andygobrien@gmail.com>
2018-01-09 18:32:58 -0500

    Equal item spacing fix

--- app/src/main/java/com/llamalabb/com/comllamalabbokcupidtakehome/search/tabs/EqualSpaceItemDecorator.kt
+++ app/src/main/java/com/llamalabb/com/comllamalabbokcupidtakehome/search/tabs/EqualSpaceItemDecorator.kt
@@ -4,16 +4,29 @@ import android.content.Context
 import android.graphics.Rect
 import android.support.v7.widget.RecyclerView
 import android.view.View
+import android.support.v7.widget.GridLayoutManager
+import com.llamalabb.com.comllamalabbokcupidtakehome.R
+import android.R.attr.spacing
+
+
+
 
 /**
  * Created by andyg on 1/7/2018.
  */
 class EqualSpaceItemDecorator(context: Context, dimenId: Int) : RecyclerView.ItemDecoration() {
 
-    private val spaceHeight = context.resources.getDimensionPixelSize(dimenId)
+    private val padding = context.resources.getDimensionPixelSize(dimenId)
+    private val spanSize = context.resources.getInteger(R.integer.gallery_columns)
 
-    override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
-        super.getItemOffsets(outRect, view, parent, state)
-        outRect.set(spaceHeight, spaceHeight, spaceHeight, spaceHeight)
+
+    override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State?) {
+        val position = parent.getChildAdapterPosition(view)
+        val column = position % spanSize
+        outRect.left = padding - column * padding / spanSize
+        outRect.right = (column + 1) * padding / spanSize
+        if (position < spanSize) outRect.top = padding
+        outRect.bottom = padding
+
     }
 }
